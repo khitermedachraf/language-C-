c5f732d59889810b81dcf7b488928b967220cef7
khitermedachraf <hm_khiter@esi.dz>
2019-12-17 01:04:45 +0100

    learning how to use files on language C Vol 2

--- openclassroom/fichiers/les fichiers/main.c
+++ openclassroom/fichiers/les fichiers/main.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define TAILLE_MAX 1000 // Tableau de taille 1000
 int main(int argc, char *argv[])
 {
     FILE* fichier = NULL;
-    fichier = fopen("test.txt", "r+");
 
+    char chaine[TAILLE_MAX] = ""; // Chaîne vide de taille TAILLE_MAX
+
+    fichier = fopen("test.txt", "r");
+    int score[3] = {0}; // Tableau des 3 meilleurs scores
     if (fichier != NULL)
     {
-        /// On peut lire et écrire dans le fichier
-        fputc('A', fichier); // Écriture du caractère A
-        fclose(fichier); /// On ferme le fichier qui a été ouvert
-        fichier = fopen("test.txt", "r+");
-        fputs("Salut les Zér0s\nComment allez-vous ?", fichier);
-        // On demande l'âge
-        printf("Quel age avez-vous ? ");
-        int age ;
-        scanf("%d", &age);
+        fscanf(fichier, "%d %d %d", &score[0], &score[1], &score[2]);
+        while (fgets(chaine, TAILLE_MAX, fichier) != NULL) // On lit le fichier tant qu'on ne reçoit pas d'erreur (NULL)
+        {
+            printf("%s", chaine); // On affiche la chaîne qu'on vient de lire
+        }
+        printf("***Les meilleurs scores sont : %d, %d et %d***", score[0], score[1], score[2]);
+        fclose(fichier);
+    }
+    ///////////////////////////////////////////////////////////
+    rename("test.txt", "test_renomme.txt");
 
-        // On l'écrit dans le fichier
-        fprintf(fichier, "Le Monsieur qui utilise le programme, il a %d ans", age);
-        fclose(fichier); /// On ferme le fichier qui a été ouvert
-    }
-    else
-    {
-        /// On affiche un message d'erreur si on veut
-        printf("Impossible d'ouvrir le fichier test.txt");
-    }
 
     return 0;
 }
